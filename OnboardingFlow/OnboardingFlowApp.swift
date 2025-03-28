8c24cd8d17966a4ffc0e66e2e91991b903bffc17
bmmuceo <bmmuceo@gmail.com>
2025-03-28 02:06:25 -0400

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by RyanA on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
